22a9b2fe17c2b1f0a55219d85b0c6ef54db77bcf
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-11-01 12:06:52 +0200

    Create clients to call open APIs

---
+++ CoinMarketCat/Clients/BaseClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+
+namespace Clients
+{
+    public abstract class BaseClient
+    {
+        public HttpClient _httpclient;
+        public string Url { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ CoinMarketCat/Clients/CoinCapClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Clients
+{
+    public class CoinCapClient : BaseClient
+    {
+        public CoinCapClient()
+        {
+            Url = "https://api.coincap.io/v2/";
+            _httpclient = new HttpClient()
+            {
+                BaseAddress = new Uri(Url)
+            };
+            _httpclient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
